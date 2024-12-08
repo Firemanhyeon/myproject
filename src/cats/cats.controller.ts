2632bb1cdb8e030037e931ab527fc74fb9f7b54d
Firemanhyeon <ghgus990129@naver.com>
2024-12-09 02:13:52 +0900

    "express"

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,21 +1,24 @@
-import { Controller, Delete, Get, HttpException, Param, ParseIntPipe, Patch, Post, Put, UseFilters } from '@nestjs/common';
+import { Controller, Delete, Get, HttpException, Param, ParseIntPipe, Patch, Post, Put, UseFilters, UseInterceptors } from '@nestjs/common';
 import { CatsService } from './cats.service';
-import { HttpExceptionFilter } from 'src/http-exception.filter';
+import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
+import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
 
 @Controller('cats')
+@UseInterceptors(SuccessInterceptor)
+@UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
-  @UseFilters(HttpExceptionFilter)
   getAllCat() {
-    throw new HttpException('api is broken', 401);
+    console.log('hello controller');
+    // throw new HttpException('api is broken', 401);
     return 'all cat';
   }
   @Get(':id')
-  getOneCat(@Param('id',ParseIntPipe) param: number) {
-      console.log(param);
-      console.log(typeof param)
+  getOneCat(@Param('id', ParseIntPipe) param: number) {
+    console.log(param);
+    console.log(typeof param);
     return 'one cat';
   }
 
